3143f0d55a2e489c259f7f857b4b114b12803196
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-10-06 11:28:05 -0300

    Add files via upload

---
+++ java/model/CriarTabelas.java
@@ -0,0 +1,9 @@
+package model;
+
+import javax.persistence.Persistence;
+
+public class CriarTabelas {
+	public static void main(String[] args) {
+		Persistence.createEntityManagerFactory("FinanceiroPU");
+		}
+}
