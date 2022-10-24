b618e4e0ba00a0a52f0ce34bf6c59dca8b54f192
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-10-24 09:25:00 -0300

    Update NomesBean.java

--- Financeiro/src/main/java/model/NomesBean.java
+++ Financeiro/src/main/java/model/NomesBean.java
@@ -8,6 +8,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+import javax.faces.component.html.HtmlInputText;
 
 @ManagedBean
 @RequestScoped
@@ -19,9 +21,24 @@ public class NomesBean {
 	private String nome;
 	private List<String> nomes = new ArrayList<>();
 	
+	private HtmlInputText inputNome;
+	private HtmlCommandButton botaoAdicionar;
+	
+	
 	public void adicionar() {
-			this.nomes.add(nome);
-}
+		this.nomes.add(nome);
+		
+		// desativa campo e botão quando mais que 3 nomes
+		// forem adicionados
+		if (this.nomes.size() > 3) {
+			this.inputNome.setDisabled(true);
+			this.botaoAdicionar.setDisabled(true);
+			this.botaoAdicionar.setValue(
+			"Muitos nomes adicionados...");
+			}
+	}
+	// getters e setters
+	
 	public String getNome() {
 			return nome;
 		}
