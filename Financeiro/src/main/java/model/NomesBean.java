f9a9dc55ed43440d0660c04b98036c4f9eb5799f
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-10-25 11:48:22 -0300

    Update NomesBean.java

--- Financeiro/src/main/java/model/NomesBean.java
+++ Financeiro/src/main/java/model/NomesBean.java
@@ -2,76 +2,59 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.component.html.HtmlCommandButton;
 import javax.faces.component.html.HtmlInputText;
 
 @ManagedBean
-@RequestScoped
 @ViewScoped
-@SessionScoped
-@ApplicationScoped
 public class NomesBean {
-	
 	private String nome;
 	private List<String> nomes = new ArrayList<>();
-	
+
 	private HtmlInputText inputNome;
 	private HtmlCommandButton botaoAdicionar;
-	
-	
+
 	public void adicionar() {
 		this.nomes.add(nome);
-		
+
 		// desativa campo e botão quando mais que 3 nomes
 		// forem adicionados
 		if (this.nomes.size() > 3) {
 			this.inputNome.setDisabled(true);
 			this.botaoAdicionar.setDisabled(true);
-			this.botaoAdicionar.setValue(
-			"Muitos nomes adicionados...");
-			}
-	}
-	// getters e setters
-	
-	
-	
-	public String getNome() {
-			return nome;
+			this.botaoAdicionar.setValue("Muitos nomes adicionados...");
 		}
-		public HtmlInputText getInputNome() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+	public HtmlInputText getInputNome() {
 		return inputNome;
 	}
-
-
 
 	public void setInputNome(HtmlInputText inputNome) {
 		this.inputNome = inputNome;
 	}
 
-
-
 	public HtmlCommandButton getBotaoAdicionar() {
 		return botaoAdicionar;
 	}
-
-
 
 	public void setBotaoAdicionar(HtmlCommandButton botaoAdicionar) {
 		this.botaoAdicionar = botaoAdicionar;
 	}
 
-
-
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
-		public List<String> getNomes() {
-			return nomes;
-		}
 }
